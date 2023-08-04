da8aaac1dfd8da7f9bcbc3a984af04af5c2edf32
jueun oh <jueun1025@naver.com>
2023-08-04 21:25:50 +0900

    이진탐색_파라메틱 서치

---
+++ zoouniak/BOJ/BOJ1654.java
@@ -0,0 +1,40 @@
+package BOJ;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ1654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        int[] lan = new int[n];
+        for(int i=0;i<n;i++){
+            lan[i]= Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(lan);
+        //자료형 long사용
+        long start = 1;
+        long end = lan[n-1];
+        long result=0;
+        while(start<=end){
+            long mid = (start+end)/2;
+            long sum=0;
+            for(int i=0;i<n;i++){
+                sum+=(lan[i]/mid);
+            }
+            if(sum>=k){
+                result=mid;
+                start=mid+1;
+            }
+            else{
+                //개수 부족
+                end=mid-1;
+            }
+
+        }
+        System.out.println(result);
+    }
+}
