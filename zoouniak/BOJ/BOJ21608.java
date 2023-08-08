fb6fc5df590b3cc3f0b9cb642b0574db9687bd10
jueun oh <jueun1025@naver.com>
2023-08-08 15:36:14 +0900

    [구현] 상어 초등학교

--- zoouniak/BOJ/BOJ21608.java
+++ zoouniak/BOJ/BOJ21608.java
@@ -9,7 +9,7 @@ public class BOJ21608 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int N = n*n;
+        int N = n * n;
         STUDENT[] student = new STUDENT[N + 1];
         int[][] s = new int[n + 1][n + 1];
         int[] dx = {1, -1, 0, 0};
@@ -17,7 +17,7 @@ public class BOJ21608 {
         for (int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
             int num = Integer.parseInt(input[0]);
-            student[num]=new STUDENT();
+            student[num] = new STUDENT();
             for (int j = 0; j < 4; j++) {
                 student[num].like.add(Integer.parseInt(input[j + 1]));
             }
@@ -28,50 +28,53 @@ public class BOJ21608 {
                 continue;
             }
             //끝에서부터 돌면서 좋아하는 사람이 가장 많이 인접한 칸을 찾자
-            int maxLike = 0; int maxVacant=0;
-            int R=0;int C=0;
+            int maxLike = 0;
+            int maxVacant = 0;
+            int R = 0;
+            int C = 0;
             for (int x = n; x > 0; x--) {
                 for (int y = n; y > 0; y--) {
                     int k = 0;
-                    int vacant=0;
-                    if(s[x][y]==0) {
+                    int vacant = 0;
+                    if (s[x][y] == 0) {
                         for (int z = 0; z < 4; z++) {
                             int nx = x + dx[z];
                             int ny = y + dy[z];
                             if (nx >= 1 && nx <= n && ny >= 1 && ny <= n) {
-                                if(student[num].like.contains(s[nx][ny]))
-                                //자리 범위 내, 좋아하는 친구가 잇다묜...
-                                k++;
-                                else if(s[nx][ny]==0)
+                                if (student[num].like.contains(s[nx][ny]))
+                                    //자리 범위 내, 좋아하는 친구가 잇다묜...
+                                    k++;
+                                else if (s[nx][ny] == 0)
                                     vacant++;//비어있는 칸
                             }
                             //인접 4칸 다 돌기.
                         }
-                        if (maxLike < k ) {
+                        if (maxLike < k) {
                             maxLike = k;
-                            maxVacant=vacant;
-                            R=x;C=y;
-                        }
-                        else if(maxLike==k && vacant>maxVacant){
-                            R=x;C=y;
-                            maxVacant=vacant;
-                        }
-                        else if(maxLike ==k && maxVacant==vacant){
-                            R=x;C=y;
+                            maxVacant = vacant;
+                            R = x;
+                            C = y;
+                        } else if (maxLike == k && vacant > maxVacant) {
+                            R = x;
+                            C = y;
+                            maxVacant = vacant;
+                        } else if (maxLike == k && maxVacant == vacant) {
+                            R = x;
+                            C = y;
                         }
                     }
                 }
             }
-            student[num].setSeat(R,C);
+            student[num].setSeat(R, C);
             student[num].setSatisfy(maxLike);
-            s[R][C]=num;
+            s[R][C] = num;
         }
-        int result=0;
-        for(int i=1;i<N+1;i++){
-            int k=0;
+        int result = 0;
+        for (int i = 1; i < N + 1; i++) {
+            int k = 0;
             int r = student[i].getSeat().r;
             int c = student[i].getSeat().c;
-            for(int j=0;j<4;j++) {
+            for (int j = 0; j < 4; j++) {
                 int nx = r + dx[j];
                 int ny = c + dy[j];
                 if (nx >= 1 && nx <= n && ny >= 1 && ny <= n && student[i].like.contains(s[nx][ny])) {
@@ -99,10 +102,9 @@ public class BOJ21608 {
     }
 
     public static class STUDENT {
-        //좋아하는 학생
         ArrayList<Integer> like = new ArrayList<>(4);
-        Seat seat=new Seat();
-        int satisfy=0;
+        Seat seat = new Seat();
+        int satisfy = 0;
 
         public void setSeat(int r, int c) {
             this.seat.r = r;
