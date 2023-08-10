e9865ec47c22a44ebdd88f940ac4dd5d04807169
jueun oh <jueun1025@naver.com>
2023-08-10 14:32:05 +0900

    [그리디] Yonsei TOTO (별도 클래스 생성x)

--- zoouniak/BOJ/BOJ12018.java
+++ zoouniak/BOJ/BOJ12018.java
@@ -12,7 +12,7 @@ public class BOJ12018 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
         int n = Integer.parseInt(s[0]);
-        Class[] classes = new Class[n];
+        ArrayList<Integer>[] classes = new ArrayList[n];
         int[] need = new int[n];
         int m = Integer.parseInt(s[1]);
         for (int i = 0; i < n; i++) {
@@ -20,12 +20,17 @@ public class BOJ12018 {
             //신청인원을 마일리지 순으로 정렬하고 수강인원 l번째 사람과 같은 마일리지를 가지면된다
             int p = Integer.parseInt(s2[0]);
             int l = Integer.parseInt(s2[1]);
-            classes[i] = new Class(p, l);
 
             String[] mileage = br.readLine().split(" ");
-            for (String value : mileage) classes[i].people.add(Integer.valueOf(value));
-            classes[i].sortByMileage();
-            need[i] = classes[i].needMileage();//각 클래스마다 필요한 마일리지 구하기
+            classes[i] = new ArrayList<>();
+            for (String value : mileage) {
+                classes[i].add(Integer.parseInt(value));
+            }
+            classes[i].sort(Comparator.reverseOrder());
+            if(l>p) need[i]=1;
+            else{
+                need[i]=classes[i].get(l-1);
+            }
         }
         Arrays.sort(need);//필요한 마일리지가 적은 거부터
         int result = 0;
@@ -38,24 +43,4 @@ public class BOJ12018 {
         System.out.println(result);
     }
 
-    public static class Class {
-        int register;
-        int limit;
-
-        ArrayList<Integer> people = new ArrayList<>();
-
-        public Class(int register, int limit) {
-            this.register = register;
-            this.limit = limit;
-        }
-
-        public void sortByMileage() {
-            people.sort(Comparator.reverseOrder());
-        }
-
-        public int needMileage() {
-            if (limit > register) return 1;
-            return people.get(this.limit - 1);
-        }
-    }
 }
