6c9eedcf5c9ae62f5ac833595ebf5d5e97b6d1cb
jueun oh <jueun1025@naver.com>
2023-08-16 23:14:23 +0900

    [BFS] 미로 탐색

--- zoouniak/BOJ/BOJ2178.java
+++ zoouniak/BOJ/BOJ2178.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ2178 {
-    static boolean[][] visited;
     static int[][] map;
     static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,1,-1};
@@ -14,7 +13,6 @@ public class BOJ2178 {
         String[] s = br.readLine().split(" ");
         n = Integer.parseInt(s[0]);
         m = Integer.parseInt(s[1]);
-        visited = new boolean[n][m];
         map = new int[n][m];
         for(int i=0;i<n;i++){
             String input = br.readLine();
@@ -26,19 +24,12 @@ public class BOJ2178 {
                 else map[i][j]=0;
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(map[i][j]==1&&!visited[i][j]){
-                    bfs(i,j);
-                }
-            }
-        }
+        bfs(0,0);
         System.out.println(map[n-1][m-1]);
     }
     public static void bfs(int x,int y){
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[] {x,y});
-        visited[x][y] =true;
         while(!q.isEmpty()){
             int X = q.peek()[0];
             int Y = q.peek()[1];
@@ -46,8 +37,7 @@ public class BOJ2178 {
             for(int i=0;i<4;i++){
                 int nx = X+dx[i];
                 int ny = Y+dy[i];
-                if(nx>=0&&nx<n&&ny>=0&&ny<m&&map[nx][ny]==1&&!visited[nx][ny]){
-                    visited[nx][ny]=true;
+                if(nx>=0&&nx<n&&ny>=0&&ny<m&&map[nx][ny]==1){
                     q.add(new int[] {nx,ny});
                     map[nx][ny]=map[X][Y]+1;
                 }
