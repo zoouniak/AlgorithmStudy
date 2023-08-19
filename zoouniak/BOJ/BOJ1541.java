c0377006d12e892fa0ccaf917faa93252f0781d5
jueun oh <jueun1025@naver.com>
2023-08-19 23:24:01 +0900

    [그리디] 잃어버린 괄호

--- zoouniak/BOJ/BOJ1541.java
+++ zoouniak/BOJ/BOJ1541.java
@@ -8,22 +8,28 @@ public class BOJ1541 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
+        String[] nums = s.split("\\+|-");
+        int numCnt = 0;
         int sum = 0;
-        String num = "0";
-        boolean minus = false;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) != '+' && s.charAt(i) != '-') {
-                num += s.charAt(i);
-            }
-            if (s.charAt(i) == '+'||i==s.length()-1||s.charAt(i)=='-') {
-                if (minus) sum -= Integer.parseInt(num);
-                else sum += Integer.parseInt(num);
-                num = "";
-                if(s.charAt(i)=='-') minus=true;
-            }
+        sum += Integer.parseInt(nums[numCnt++]);
 
+        char lastOp = '+';
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '+') {
+                if (lastOp == '-') {
+                    sum -= Integer.parseInt(nums[numCnt++]);
+                } else {
+                    //뒤에 숫자가 0으로 시작하면 0만 더하고 괄호를 쳐버리자?
+                    sum += Integer.parseInt(nums[numCnt++]);
+                    lastOp = '+';
+                }
+
+            } else if (s.charAt(i) == '-') {
+                //빼기인 경우
+                sum -= Integer.parseInt(nums[numCnt++]);
+                lastOp = '-';
+            }
         }
         System.out.println(sum);
-
     }
 }
