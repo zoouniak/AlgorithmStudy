2466528b4516bc4a313936cba1a7c76c47c85682
jueun oh <jueun1025@naver.com>
2023-08-21 23:10:38 +0900

    [구현,그리디] 폴리오미노

---
+++ zoouniak/BOJ/python/BOJ1343.py
@@ -0,0 +1,24 @@
+import sys
+
+b = list(input())
+count = 0
+result = ""
+for i in b:
+    if i == "X":
+        count += 1
+    else:
+        if (count % 2 == 0):
+            result = result + "AAAA" * (count // 4)
+            result = result + "B" * (count % 4)
+        else:
+            print(-1)
+            sys.exit()
+        count=0
+        result = result + "."
+if (count % 2 == 0):
+    result = result + "AAAA" * (count // 4)
+    result = result + "B" * (count % 4)
+else:
+    print(-1)
+    sys.exit()
+print(result)
