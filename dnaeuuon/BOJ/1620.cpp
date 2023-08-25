178ad243d4099eb538d3d024151a265245479c82
dnaeuuon <123866834+bye-on@users.noreply.github.com>
2023-08-25 23:14:09 +0900

    [해시를 사용한 집합과 맵] 나는야 포켓몬 마스터 이다솜
    맵을 두 개 썼다..!
    이렇게 하니까 확실히 for문을 돌지 않아 바로 시간초과 해결함.

--- dnaeuuon/BOJ/1620.cpp
+++ dnaeuuon/BOJ/1620.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <math.h>
+#include <map>
 using namespace std;
-string str[100001];
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
+	map<int, string> hash1;
+	map<string, int> hash2;
 
 	int N, M;
 	cin >> N >> M;
@@ -13,25 +15,20 @@ int main() {
 	for (int i = 1; i <= N; i++) {
 		string input;
 		cin >> input;
-		str[i] = input;
+		hash1.insert(make_pair(i, input));
+		hash2.insert(make_pair(input, i));
 	}
 	for (int i = 0; i < M; i++) {
 		string input;
 		cin >> input;
 		int num = 0;
 		for (int j = 0; j < input.length(); j++) {
-			if(isdigit(input[j]))
+			if (isdigit(input[j]))
 				num += (input[j] - '0') * pow(10, input.length() - 1 - j);
 		}
-		if (num != 0) cout << str[num] << '\n';
+		if (num != 0) cout << hash1[num] << '\n';
 		else {
-			for (int j = 1; j <= N; j++) {
-				int result = str[j].compare(input);
-				if (result == 0) {
-					cout << j << '\n';
-					break;
-				}
-			}
+			cout << hash2[input] << '\n';
 		}
 	}
 }
