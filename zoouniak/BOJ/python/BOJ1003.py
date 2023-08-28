a0c8841582fb3f0b99093051b3e73478223c78d0
jueun oh <jueun1025@naver.com>
2023-08-28 23:42:15 +0900

    [DP] 피보나치 함수

---
+++ zoouniak/BOJ/python/BOJ1003.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+
+def fibo(n):
+    dp = {0: [1, 0], 1: [0, 1]}
+    for i in range(2, n + 1):
+        dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]]
+    print(dp[n][0], dp[n][1])
+
+for _ in range(t):
+    n = int(input())
+    fibo(n)
