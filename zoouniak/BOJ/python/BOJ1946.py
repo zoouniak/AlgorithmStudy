1737b2f8b9a8dc5045526c7cba97d9e075c35364
jueun oh <jueun1025@naver.com>
2023-09-04 22:03:27 +0900

    [그리디] 신입사원 배열을 이용하여 좀 더 compact

--- zoouniak/BOJ/python/BOJ1946.py
+++ zoouniak/BOJ/python/BOJ1946.py
@@ -3,15 +3,15 @@ import sys
 input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
-    lst = []
     n = int(input())
+    person = [0 for _ in range(n + 1)]
     for _ in range(n):
-        lst.append(list(map(int, input().split())))
-    lst.sort()
+        a, b = map(int, input().split())
+        person[a] = b
     cnt = 1
-    Bgrade = lst[0][1]  # A 1등의 B성적.
-    for i in range(1, n):
-        if Bgrade > lst[i][1]:  # 나보다 A순위 높은 사람보다 B순위 높으면 ok
+    Bgrade = person[1]  # A 1등의 B성적.
+    for i in range(1, n + 1):
+        if Bgrade > person[i]:  # 나보다 A순위 높은 사람보다 B순위 높으면 ok
             cnt += 1
-            Bgrade = lst[i][1]
+            Bgrade = person[i]
     print(cnt)
