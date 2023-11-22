b74b4f1379844de2843a9a9579bbf5bf24a7596b
jueun oh <jueun1025@naver.com>
2023-11-23 01:40:13 +0900

    [DFS,BFS] 인접행렬로 구현

--- zoouniak/BOJ/java/BOJ1260.java
+++ zoouniak/BOJ/java/BOJ1260.java
@@ -3,14 +3,10 @@ package BOJ.java;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class BOJ1260 {
-    //인접 리스트로 구현
-    public static ArrayList<Integer>[] graph;
+    public static int[][] graph;
     public static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -20,21 +16,15 @@ public class BOJ1260 {
         int m = Integer.parseInt(input[1]);
         int v = Integer.parseInt(input[2]);
 
-        graph = new ArrayList[n + 1];
-        visited = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < m; i++) {
-            input = br.readLine().split(" ");
-            int a = Integer.parseInt(input[0]);
-            int b = Integer.parseInt(input[1]);
-            graph[a].add(b);
-            graph[b].add(a);
-        }
-        for (int i = 1; i <= n; i++) {
-            graph[i].sort(Comparator.naturalOrder());
-        }
+       graph = new int[n+1][n+1];
+       visited = new boolean[n+1];
+       for(int i=0;i<m;i++){
+           input = br.readLine().split(" ");
+           int a = Integer.parseInt(input[0]);
+           int b = Integer.parseInt(input[1]);
+           graph[a][b]=1;
+           graph[b][a]=1;
+       }
 
         dfs(v);
         System.out.println();
@@ -43,28 +33,27 @@ public class BOJ1260 {
     }
 
     public static void dfs(int v) {
-        //깊이 우선..
+        //깊이 우선
         System.out.print(v + " ");
         visited[v] = true;
-        for (int vertex : graph[v]) {
-            if (!visited[vertex]) {
-                dfs(vertex);
+        for(int i=1;i<graph[v].length;i++){
+            if(graph[v][i]==1 && !visited[i]){
+                dfs(i);
             }
         }
     }
     public static void bfs(int v){
-        //내 인접한 것 먼저..
         Queue<Integer> q = new LinkedList<>();
         visited[v]=true;
         q.add(v);
         System.out.print(v+" ");
         while (!q.isEmpty()){
             int x = q.poll();
-            for (Integer vertex : graph[x]) {
-                if(!visited[vertex]){
-                    visited[vertex]=true;
-                    q.add(vertex);
-                    System.out.print(vertex+" ");
+            for(int i=1;i<graph[x].length;i++){
+                if(graph[x][i]==1 && !visited[i]){
+                    visited[i]=true;
+                    System.out.print(i+" ");
+                    q.add(i);
                 }
             }
         }
