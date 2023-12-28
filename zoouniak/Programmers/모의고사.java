bbdb8f39ca15e520583a94cf2db88cff66b220d9
jueun oh <jueun1025@naver.com>
2023-12-28 22:56:43 +0900

    [완전탐색]

--- zoouniak/Programmers/모의고사.java
+++ zoouniak/Programmers/모의고사.java
@@ -10,18 +10,16 @@ public class 모의고사 {
         int[] p1 = {1, 2, 3, 4, 5};
         int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int c1 = 0;
-        int c2 = 0;
-        int c3 = 0;
+        int[] count = new int[3];
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == p1[i % 5]) c1++;
-            if (answers[i] == p2[i % 8]) c2++;
-            if (answers[i] == p3[i % 10]) c3++;
+            if (answers[i] == p1[i % p1.length]) count[0]++;
+            if (answers[i] == p2[i % p2.length]) count[1]++;
+            if (answers[i] == p3[i % p3.length]) count[2]++;
         }
-        int max = Math.max(c1, Math.max(c2, c3));
-        if (c1 == max) list.add(1);
-        if (c2 == max) list.add(2);
-        if (c3 == max) list.add(3);
+        int max = Math.max(count[0], Math.max(count[1], count[2]));
+        for (int i = 0; i < 3; i++) {
+            if (count[i] == max) list.add(i + 1);
+        }
 
         list.sort(Comparator.naturalOrder());
         list.stream().mapToInt(Integer::intValue).toArray();
